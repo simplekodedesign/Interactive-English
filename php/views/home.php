f8fac739b3a1749dc118e42f1317c34e1a4b533a
Daniel Salazar <dnlsalazar27@gmail.com>
2019-09-16 22:36:39 -0400

    Añadiendo botones de siguiente y anterior

--- php/views/home.php
+++ php/views/home.php
@@ -100,27 +100,27 @@
 
         <div class="container" id="road">
           <div id="lessons">
-            <div  class="roadicon" id="theme1"><img src="../../img/lessons/background/abc.svg" width="100%" alt="The Alphabet"></div>
-            <div  class="roadicon" id="theme2"><img src="../../img/lessons/background/greetings.svg" height="100%" alt="The Greetings"></div>
-            <div  class="roadicon" id="theme3"><img src="../../img/lessons/background/thenumbers.svg" height="100%" alt="The Numbers"></div>
-            <div  class="roadicon" id="theme4"><img src="../../img/lessons/background/personalinformation.svg" height="60%" alt="Personal information"></div>
-            <div  class="roadicon" id="theme5"><img src="../../img/lessons/background/personalpronouns.svg" height="100%" alt="Personal pronouns"></div>
-            <div  class="roadicon" id="theme6"><img src="../../img/lessons/background/animals.svg" height="100%" alt="Animals"></div>
-            <div  class="roadicon" id="theme7"><img src="../../img/lessons/background/foodanddrinks.svg" height="100%" alt="Food and drinks"></div>
-            <div  class="roadicon" id="theme8"><img src="../../img/lessons/background/body.svg" height="100%" alt="Parts of the body"></div>
-            <div  class="roadicon" id="theme11"><img src="../../img/lessons/background/plurals.svg" height="100%" alt="Plurals"></div>
-            <div  class="roadicon" id="theme9"><img src="../../img/lessons/background/thisthese.svg" height="100%" alt="This-These"></div>
-            <div  class="roadicon" id="theme10"><img src="../../img/lessons/background/thatthose.svg" height="100%" alt="That-thosea"></div>
-            <div  class="roadicon" id="theme12"><img src="../../img/lessons/background/thecolors.svg" height="70%" alt="Colors"></div>
-            <div  class="roadicon" id="theme13"><img src="../../img/lessons/background/occupations.svg" height="100%" alt="To be and occupations"></div>
-            <div  class="roadicon" id="theme14"><img src="../../img/lessons/background/sports.svg" height="100%" alt="Sports"></div>
-            <div  class="roadicon" id="theme15"><img src="../../img/lessons/background/leccion 16.svg" height="100%" alt="Sports"></div>
-            <div  class="roadicon" id="theme16"><img src="../../img/lessons/background/directions.svg" height="80%" alt="Directions"></div>
-            <div  class="roadicon" id="theme17"><img src="../../img/lessons/background/family.svg" height="80%" alt="Family"></div>
-            <div  class="roadicon" id="theme18"><img src="../../img/lessons/background/hours.svg" height="70%" alt="The hours"></div>
-            <div id="start"><img src="../../img/lessons/background/start.svg" height="100%" alt="Start"></div>
-            <div id="end"><img src="../../img/lessons/background/end.svg" height="100%" alt="END"></div>
-            <img src="../../img/lessons/background/Recorrido.svg" alt="camino" width="100%">
+          <div  class="roadicon" id="theme1"><img src="../../img/lessons/background/abc.svg" width="100%" alt="The Alphabet"></div>
+          <div  class="roadicon" id="theme2"><img src="../../img/lessons/background/greetings.svg" height="100%" alt="The Greetings"></div>
+          <div  class="roadicon" id="theme3"><img src="../../img/lessons/background/thenumbers.svg" height="100%" alt="The Numbers"></div>
+          <div  class="roadicon" id="theme4"><img src="../../img/lessons/background/personalinformation.svg" height="60%" alt="Personal information"></div>
+          <div  class="roadicon" id="theme5"><img src="../../img/lessons/background/articles.svg" height="100%" alt="Articles (a, an, the)"></div>
+          <div  class="roadicon" id="theme6"><img src="../../img/lessons/background/animals.svg" height="100%" alt="Animals"></div>
+          <div  class="roadicon" id="theme7"><img src="../../img/lessons/background/foodanddrinks.svg" height="100%" alt="Food and drinks"></div>
+          <div  class="roadicon" id="theme8"><img src="../../img/lessons/background/body.svg" height="100%" alt="Parts of the body"></div>
+          <div  class="roadicon" id="theme11"><img src="../../img/lessons/background/plurals.svg" height="100%" alt="plurals"></div>
+          <div  class="roadicon" id="theme9"><img src="../../img/lessons/background/thisthese.svg" height="100%" alt="This-These"></div>
+          <div  class="roadicon" id="theme10"><img src="../../img/lessons/background/thatthose.svg" height="100%" alt="That-thosea"></div>
+          <div  class="roadicon" id="theme12"><img src="../../img/lessons/background/thecolors.svg" height="70%" alt="Colors"></div>
+          <div  class="roadicon" id="theme13"><img src="../../img/lessons/background/occupations.svg" height="100%" alt="To be and occupations"></div>
+          <div  class="roadicon" id="theme14"><img src="../../img/lessons/background/sports.svg" height="100%" alt="Sports"></div>
+          <div  class="roadicon" id="theme15"><img src="../../img/lessons/background/prepositions.svg" height="100%" alt="Prepositions"></div>
+          <div  class="roadicon" id="theme16"><img src="../../img/lessons/background/directions.svg" height="80%" alt="Directions"></div>
+          <div  class="roadicon" id="theme17"><img src="../../img/lessons/background/family.svg" height="80%" alt="Family"></div>
+          <div  class="roadicon" id="theme18"><img src="../../img/lessons/background/hours.svg" height="70%" alt="The hours"></div>
+          <div id="start"><img src="../../img/lessons/background/start.svg" height="100%" alt="Start"></div>
+          <div id="end"><img src="../../img/lessons/background/end.svg" height="100%" alt="END"></div>
+          <img src="../../img/lessons/background/Recorrido.svg" alt="camino" width="100%">
           </div>
         </div>
       </section>
@@ -138,13 +138,13 @@
         </div>";
 
         if($lant != "" && $lant != "#"){
-          echo "<div><a href='".$lant."'>Antes</a></div>";
+          echo "<div class='navigationButtons' id='lPrev'><a href='".$lant."'><span></span></a></div>";
         }
         if($lact != "" && $lact != "#"){
           echo "<div><a href='".$lact."'>Actual</a></div>";
         }
         if($lsig != "" && $lsig != "#"){
-          echo "<div><a href='".$lsig."'>Siguiente</a></div>";
+          echo "<div class='navigationButtons' id='lSigu'><a href='".$lsig."'><span></span></a></div>";
         }
       }
     ?>
