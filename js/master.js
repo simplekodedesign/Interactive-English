0d28ee1aef61fb15c84f2432bd7f9325d275b423
DiegoSalazar27 <diegosalazargm@gmail.com>
2019-09-19 07:58:20 -0400

    for pulling

--- js/master.js
+++ js/master.js
@@ -1,16 +1,18 @@
-const color = [
-  "rgb(248, 206, 0)",
-  "rgb(237, 112, 41)",
-  "rgb(233, 79, 136)",
-  "rgb(0, 184, 231)",
-  "rgb(0, 123, 191)",
-  "rgb(148, 145, 198)",
-  "rgb(0, 172, 146)"
-];
+window.addEventListener("load", function() {
+  const color = [
+    "rgb(248, 206, 0)",
+    "rgb(237, 112, 41)",
+    "rgb(233, 79, 136)",
+    "rgb(0, 184, 231)",
+    "rgb(0, 123, 191)",
+    "rgb(148, 145, 198)",
+    "rgb(0, 172, 146)"
+  ];
 
-let svgcolor = numero_leccion-1;
-if (svgcolor >= 7) {
-  svgcolor%= 7;
-}
+  let svgcolor = numero_leccion-1;
+  if (svgcolor >= 7) {
+    svgcolor%= 7;
+  }
 
-document.documentElement.style.setProperty('--lesson', color[svgcolor]);
+  document.documentElement.style.setProperty('--lesson', color[svgcolor]);
+});
