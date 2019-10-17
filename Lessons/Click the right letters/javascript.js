1bf37572eac9470559f9bc641273c686040a16b8
Daniel Salazar <dnlsalazar27@gmail.com>
2019-10-16 21:40:10 -0500

    Finishing with click the right letters with images

--- Lessons/Click the right letters/javascript.js
+++ Lessons/Click the right letters/javascript.js
@@ -1,41 +1,84 @@
 var mainLetter = document.getElementById("mainLetter");
 const audioURL = cord == 26? "../../aud/categories/numbers/" : "../../aud/categories/abc/";
-const lettersInit = cord == 26? 1 : 65;
+const type = data.urlImg[0]? 1 : 0;
+if (type == 1) {
+  var lettersInit = 0;
+  var lettersLength = data.urlImg.length;
+} else {
+  var lettersInit = cord == 26? 1 : 65;
+  var lettersLength = cord == 26? lettersInit + 22 : lettersInit + 26;
+}
+
 const mistakeAud = document.getElementById("mistakeAud");
-var lettersLength = cord == 26? lettersInit + 22 : lettersInit + 26;
 var posLetter = lettersInit;
 var mainLetter = document.getElementById("mainLetter");
 var optionsLetters = document.getElementById("optionsLetters");
 var appear, appearInit;
 var mainAud = document.getElementById("mainAud");
 
-var optionsLength = 20;
+var optionsLength = data.urlImg[0]? 10 : 20;
 
 window.addEventListener("load", function() {
-  // for (let k = 0; k < optionsLength; k++) {
-  //   let randPos = parseInt(getRandomArbitrary(65, 91));
-  // }
-  cord == 26? createGame(0) : createGame(1);
+  if(type == 1) {
+    createGame(2);
+  } else {
+    cord == 26? createGame(0) : createGame(1);
+  }
 });
 
 function createGame (num) {
   var div;
+  var img;
   appearInit = parseInt(getRandomArbitrary(4, 7));
   appear = appearInit;
-
   if(posLetter == lettersInit) {
-    for (let i = 0; i < optionsLength; i++) {
+    if(num == 2) {
+      img = document.createElement("img");
+      mainLetter.insertBefore(img, mainLetter.firstElementChild);
+      console.log(mainLetter);
+      alert(mainLetter.firstElementChild);
+    
+    for (let h = 0; h < optionsLength; h++) {
       div = document.createElement("div");
+      img = document.createElement("img");
       div.classList.add("letter");
-      div.innerHTML = "0";
+      img.setAttribute("src", "nothing");
+      img.setAttribute("alt", "0");
+      console.log(img);
+      alert(img);
+      div.appendChild(img);
       div.addEventListener("click", check);
       optionsLetters.appendChild(div);
     }
-    optionsLetters = document.getElementsByClassName("letter");
+
+      optionsLetters = document.getElementsByClassName("letter");
+      console.log(optionsLetters);
+      alert("You're out " + optionsLetters.length);
+
+    } else {
+      for (let i = 0; i < optionsLength; i++) {
+        div = document.createElement("div");
+        div.classList.add("letter");
+        div.innerHTML = "0";
+        div.addEventListener("click", check);
+        optionsLetters.appendChild(div);
+        console.log(div);
+        alert(div);
+      }
+      optionsLetters = document.getElementsByClassName("letter");
+    }
   } else {
-    for (var j = 0; j < optionsLength; j++) {
-      optionsLetters[j].removeAttribute('style');
-      optionsLetters[j].innerHTML = "0";
+    if (num == 2) {
+      // for (var j = 0; j < optionsLength; j++) {
+      //   optionsLetters[j].firstElementChild.removeAttribute('style');
+      //   optionsLetters[j].firstElementChild.src = "";
+      //   optionsLetters[j].firstElementChild.alt = "0";
+      // }
+    } else {
+      for (var j = 0; j < optionsLength; j++) {
+        optionsLetters[j].removeAttribute('style');
+        optionsLetters[j].innerHTML = "0";
+      }
     }
   }
 
@@ -48,7 +91,6 @@ function createGame (num) {
       let randPos = parseInt(getRandomArbitrary(0, optionsLength));
       if(optionsLetters[randPos].innerHTML == "0") {
         optionsLetters[randPos].innerHTML = mainLetter.innerHTML;
-        // optionsLetters[randPos].style.setProperty("color", "red");
         i++;
       }
     }
@@ -71,7 +113,6 @@ function createGame (num) {
       let randPos = parseInt(getRandomArbitrary(0, optionsLength));
       if(optionsLetters[randPos].innerHTML == "0") {
         optionsLetters[randPos].innerHTML = mainLetter.innerHTML;
-        // optionsLetters[randPos].style.setProperty("color", "red");
         i++;
       }
     }
@@ -86,32 +127,74 @@ function createGame (num) {
         optionsLetters[k].innerHTML = char;
       }
     }
+  } else if (num == 2) {
+    mainLetter.firstElementChild.src =  data.urlImg[posLetter];
+    mainLetter.firstElementChild.alt =  data.compare[posLetter];
+    mainAud.src = data.urlAud;
+    console.log(optionsLetters);
+    console.log(optionsLetters[0]);
+    console.log(optionsLetters[optionsLength-1]);
+    alert("2");
+    while (i < appearInit) {
+      let randPos = parseInt(getRandomArbitrary(0, optionsLength));
+      if(optionsLetters[randPos].firstElementChild.alt == "0") {
+        optionsLetters[randPos].firstElementChild.src = mainLetter.firstElementChild.src;
+        optionsLetters[randPos].firstElementChild.alt = mainLetter.firstElementChild.alt;
+        i++;
+      }
+    }
+    alert("3" + optionsLength);
+    for (let k = 0; k < optionsLength; k++) {
+      var char;
+      do {
+        char = parseInt(getRandomArbitrary(lettersInit, lettersLength));
+      }while (data.compare[char] == mainLetter.firstElementChild.alt);
+  
+      if(optionsLetters[k].firstElementChild.alt == "0") {
+        optionsLetters[k].firstElementChild.alt = data.compare[char];
+        optionsLetters[k].firstElementChild.src = data.urlImg[char];
+      }
+    }
   }
 
 }
 
 function clearGame () {
-  for (var j = 0; j < optionsLength; j++) {
-    optionsLetters[j].removeAttribute('style');
-    optionsLetters[j].addEventListener("click", check);
+  if (type == 1) {
+    for (var j = 0; j < optionsLength; j++) {
+      optionsLetters[j].firstElementChild.removeAttribute('style');
+      optionsLetters[j].addEventListener("click", check);
+    }
+  } else {
+    for (var j = 0; j < optionsLength; j++) {
+      optionsLetters[j].removeAttribute('style');
+      optionsLetters[j].addEventListener("click", check);
+    }
   }
   appear = appearInit;
 }
 
 function check () {
   if (mainLetter.innerHTML == this.innerHTML) {
-    this.style.setProperty("color", "green");
+    if (type) {
+      this.firstElementChild.setProperty("opacity", ".7");
+    } else {
+      this.style.setProperty("color", "green");
+    }
     this.removeEventListener("click", check);
     mainAud.play();
     appear--;
   } else {
-    this.style.setProperty("color", "red");
+    if (!type) {
+      this.style.setProperty("color", "red");
+    }
     mistakeAud.play();
     setTimeout(function () {
       clearGame();
     }, 100);
     return;
   }
+
   if(appear == 0) {
     posLetter++;
     if(posLetter < lettersLength) {
