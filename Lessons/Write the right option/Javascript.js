861d6580f4fe6dcee6ff4baad839cecb1c855b22
DiegoSalazar27 <diegosalazargm@gmail.com>
2019-10-29 20:33:25 -0400

    Debugging

--- Lessons/Write the right option/Javascript.js
+++ Lessons/Write the right option/Javascript.js
@@ -3,36 +3,38 @@ var cad = "";
 var cad2 = "";
 var cad3 = "";
 var text = document.getElementById("write");
+var letra = document.getElementById("letra");
 
 window.addEventListener("load",function(){
   actual();
 	document.getElementById("btnListen").addEventListener("click", audListen);
-  
+
 	text.addEventListener("keyup", key);
+  letra.addEventListener("click", audListen);
 })
 
-function audListen () {
-  let audio = document.getElementById("aud");
-  audio.play();
-}
+  function audListen () {
+    let audio = document.getElementById("aud");
+    audio.play();
+  }
 
-function key (e)  {
-  if(text.value.length > 0){
-    cad= text.value.substr(0, text.value.length - 1);
-    cad2=text.value.substr(text.value.length-1,text.value.length);
-    text.value=cad + cad2.toLowerCase();
-  }
-  if(text.value.indexOf(" i ")>-1){
-    cad=text.value.substr(0,text.value.indexOf(" i "));
-    cad2=text.value.substr(text.value.indexOf(" i "),text.value.indexOf(" i ")+3);
-    cad3=text.value.substr(text.value.indexOf(" i ")+3,text.value.length);
-    text.value=cad+cad2.toUpperCase()+cad3;
-  }else if(text.value.indexOf("i ")>-1&&text.value.length==2){
-    cad=text.value.substr(0,text.value.indexOf("i "));
-    cad2=text.value.substr(text.value.indexOf("i "),text.value.indexOf(" i ")+3);
-    cad3=text.value.substr(text.value.indexOf("i ")+3,text.value.length);
-    text.value=cad+cad2.toUpperCase()+cad3;
-  }
+  function key (e)  {
+    if(text.value.length > 0){
+      cad= text.value.substr(0, text.value.length - 1);
+      cad2=text.value.substr(text.value.length-1,text.value.length);
+      // text.value=cad + cad2.toLowerCase();
+    }
+    if(text.value.indexOf(" i ")>-1){
+      cad=text.value.substr(0,text.value.indexOf(" i "));
+      cad2=text.value.substr(text.value.indexOf(" i "),text.value.indexOf(" i ")+3);
+      cad3=text.value.substr(text.value.indexOf(" i ")+3,text.value.length);
+      text.value=cad+cad2.toUpperCase()+cad3;
+    }else if(text.value.indexOf("i ")>-1&&text.value.length==2){
+      cad=text.value.substr(0,text.value.indexOf("i "));
+      cad2=text.value.substr(text.value.indexOf("i "),text.value.indexOf(" i ")+3);
+      cad3=text.value.substr(text.value.indexOf("i ")+3,text.value.length);
+      text.value=cad+cad2.toUpperCase()+cad3;
+    }
 
   if(e.keyCode == 13){
     if(text.value[text.value.length-1]=="i"&&text.value[text.value.length-2]==" "){
