37bf7430df235bf455c659974d5a4a8447f0d5b7
DiegoSalazar27 <diegosalazargm@gmail.com>
2020-03-05 19:18:05 -0400

    last changes, Sweet Alert

--- js/registro.js
+++ js/registro.js
@@ -1,21 +1,29 @@
 var xhttp = new XMLHttpRequest()
 xhttp.onreadystatechange = function(){
-  if(this.readyState == 4 && this.status == 200){
-    let resp = JSON.parse(this.responseText);
-    if (resp.status === 1) {
-      Swal.fire({
-        icon: 'success',
-        title: 'Buen trabajo',
-        text: 'Has sido registrado exitosamente'
-      })}else{
+  if(this.readyState == 4){
+    if (this.status == 200) {
+      let resp = JSON.parse(this.responseText);
+      if (resp.status === 1) {
+        Swal.fire({
+          icon: 'success',
+          title: 'Buen trabajo',
+          text: 'Has sido registrado exitosamente'
+        })}else{
+          Swal.fire({
+            icon: 'error',
+            title: 'Ha ocurrido un error',
+            text: resp.message,
+          })
+        }
+    }else{
       Swal.fire({
         icon: 'error',
-        title: 'Ha ocurrido un error',
-        text: resp.message,
+        title: 'Ha ocurrido un error con la conexión',
+        text: 'Intenta recargar la pagina o contacta a soporte',
       })
-    }
+    } 
   }
- }
+}
 
 
 document.getElementById("formLogin").addEventListener("submit", e => {
